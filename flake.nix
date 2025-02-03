2687fbd2c682bb0fe79ffca821f4fa2e7dd93a55
David Millman <dave@cs.unc.edu>
2025-02-02 17:40:06 -0700

    Add flake

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "Dave Millman's personal website and blog";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = [
+          pkgs.git
+          pkgs.hugo
+
+          pkgs.nixfmt-rfc-style
+        ];
+      };
+    };
+}
